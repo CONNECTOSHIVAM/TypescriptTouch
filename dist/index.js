4b2e2d7995dc4e74db39096c7c3973a65f0b4f31
connectoshivam <connecttoshivam1@gmail.com>
2026-03-11 08:36:09 +0530

    update the files

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const name = "suhi";
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
